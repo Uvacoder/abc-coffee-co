4a0c38dd7d04e080be72ae8409432c80c1f104b7
SJellen <sjellen@me.com>
2021-06-27 12:01:07 -0400

    footer header layout set

--- components/Footer.js
+++ components/Footer.js
@@ -1,10 +1,26 @@
 import styles from '../styles/Footer.module.scss'
+import Image from 'next/image'
 
 export default function Footer() {
 
     return (
         <div className={styles.container}>
-            <h1>Footer</h1>
+            
+            <div className={styles.leftFlexContainer}>
+                <Image src="/../public/assets/shared/desktop/logo-footer.svg" alt="coffee roasters logo" width="236" height="26" />
+                <ul className={styles.ul}>
+                    <li className={styles.li}>HOME</li>
+                    <li className={styles.li}>ABOUT US</li>
+                    <li className={styles.li}>CREATE YOUR PLAN</li>
+                </ul>
+
+            </div>
+                
+            <div className={styles.rightFlexContainer}>
+                <Image src="/../public/assets/shared/desktop/icon-facebook.svg" alt="coffee roasters logo" width="24" height="24"  className={styles.svg}/>
+                <Image src="/../public/assets/shared/desktop/icon-twitter.svg" alt="coffee roasters logo" width="24" height="24"  className={styles.svg}/>
+                <Image src="/../public/assets/shared/desktop/icon-instagram.svg" alt="coffee roasters logo" width="24" height="24"  className={styles.svg}/>
+            </div>
         </div>
     )
 }
--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,18 @@
 import styles from '../styles/Header.module.scss'
+import Image from 'next/image'
 
 export default function Header() {
 
     return (
         <div className={styles.container}>
-                <h1>Header</h1>
+            <div className={styles.leftFlexContainer}>
+                <Image src="/../public/assets/shared/desktop/logo.svg" alt="coffee roasters logo" width="236" height="26"/>
+            </div>
+                <ul className={styles.ul}>
+                    <li className={styles.li}>HOME</li>
+                    <li className={styles.li}>ABOUT US</li>
+                    <li className={styles.li}>CREATE YOUR PLAN</li>
+                </ul>
         </div>
     )
 }
