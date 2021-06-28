e37b95da212618a5e9b632324656c3fbbb5696b8
SJellen <sjellen@me.com>
2021-06-28 14:19:02 -0400

    Home\WhyChoose component set.

--- components/Home/WhyChoose.js
+++ components/Home/WhyChoose.js
@@ -13,10 +13,35 @@ export default function WhyChoose() {
                     you a more impactful experience on every level.</p>
                 </div>
                 <div className={styles.aboveFlexContainer}>
-
+                    <div className={styles.featureBox}>
+                        <div className={styles.imgContainer}>
+                          <Image  src="/../public/assets/home/desktop/icon-coffee-bean.svg" alt="coffee bean icon" width="70" height="70"  className={styles.img} />  
+                        </div>
+                        <div className={styles.textContainer}>
+                            <h3>Best quality</h3>
+                        <p>Discover an endless variety of the world’s best artisan coffee from each of our roasters.</p>
+                        </div>
+                    </div>
+                    <div className={styles.featureBox}>
+                        <div className={styles.imgContainer}>
+                          <Image  src="/../public/assets/home/desktop/icon-gift.svg" alt="coffee bean icon" width="77" height="70"  className={styles.img} />  
+                        </div>
+                        <div className={styles.textContainer}>
+                           <h3>Exclusive benefits</h3>
+                            <p>Special offers and swag when you subscribe, including 30% off your first shipment.</p> 
+                        </div>
+                    </div>
+                    <div className={styles.featureBox}>
+                        <div className={styles.imgContainer}>
+                           <Image  src="/../public/assets/home/desktop/icon-truck.svg" alt="coffee bean icon" width="72" height="50"  className={styles.img} /> 
+                        </div>
+                        <div className={styles.textContainer}>
+                           <h3>Free shipping</h3>
+                            <p>We cover the cost and coffee is delivered fast. Peak freshness: guaranteed.</p> 
+                        </div>
+                        
+                    </div>
                 </div>
-                
-
             </div>
         </div>
     )
