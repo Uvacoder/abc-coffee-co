2927a52f3f675e4990623dea1b7a6ed59cb5d0b7
SJellen <sjellen@me.com>
2021-06-29 12:35:20 -0400

    About/Commitment layout done

--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,13 @@
 import styles from '../styles/About.module.scss'
 import Hero from '../components/About/Hero'
+import Commitment from '../components/About/Commitment'
 
 export default function About(){
 
         return (
             <div className={styles.container}>
                <Hero />
+               <Commitment />
             </div>
         )
 }
