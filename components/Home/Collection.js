55226f246182947522cf12804bfba255b789dffe
SJellen <sjellen@me.com>
2021-07-05 13:26:28 -0400

    Home/Collection mq's done

--- components/Home/Collection.js
+++ components/Home/Collection.js
@@ -9,24 +9,43 @@ export default function Collection() {
             <h1 className={styles.backText}>our collection</h1>
             <div className={styles.blendFlexContainer}>
                 <div className={styles.blendBox}>
-                    <Image  src="/../public/assets/home/desktop/image-gran-espresso.png" alt="coffee grounds" width="255" height="193"  className={styles.img} />
-                    <h3 className={styles.blendName}>Gran Espresso</h3>
-                    <p className={styles.description}>Light and flavorful blend with cocoa and black pepper for an intense experience</p>
+                    <div>
+                     <Image  src="/../public/assets/home/desktop/image-gran-espresso.png" alt="coffee grounds" width="255" height="193"  className={styles.img} />   
+                    </div>
+                    <div className={styles.textContainer}>
+                      <h3 className={styles.blendName}>Gran Espresso</h3>
+                    <p className={styles.description}>Light and flavorful blend with cocoa and black pepper for an intense experience</p>  
+                    </div>
+                    
                 </div>
                 <div className={styles.blendBox}>
-                    <Image  src="/../public/assets/home/desktop/image-planalto.png" alt="coffee grounds" width="255" height="193"  className={styles.img} />
-                    <h3 className={styles.blendName}>Planalto</h3>
-                    <p className={styles.description}>Brazilian dark roast with rich and velvety body, and hints of fruits and nuts</p>
+                    <div>
+                       <Image  src="/../public/assets/home/desktop/image-planalto.png" alt="coffee grounds" width="255" height="193"  className={styles.img} /> 
+                    </div>
+                    <div className={styles.textContainer}>
+                        <h3 className={styles.blendName}>Planalto</h3>
+                        <p className={styles.description}>Brazilian dark roast with rich and velvety body, and hints of fruits and nuts</p>
+                    </div>
+                    
                 </div>
                 <div className={styles.blendBox}>
-                    <Image  src="/../public/assets/home/desktop/image-piccollo.png" alt="coffee grounds" width="255" height="193"  className={styles.img} />
-                    <h3 className={styles.blendName}>Piccollo</h3>
-                    <p className={styles.description}>Mild and smooth blend featuring notes of toasted almond and dried cherry</p>
+                    <div>
+                        <Image  src="/../public/assets/home/desktop/image-piccollo.png" alt="coffee grounds" width="255" height="193"  className={styles.img} />
+                    </div>
+                    <div className={styles.textContainer}>
+                        <h3 className={styles.blendName}>Piccollo</h3>
+                        <p className={styles.description}>Mild and smooth blend featuring notes of toasted almond and dried cherry</p>
+                    </div>
                 </div>
                 <div className={styles.blendBox}>
-                    <Image  src="/../public/assets/home/desktop/image-danche.png" alt="coffee grounds" width="255" height="193"  className={styles.img} />
-                    <h3 className={styles.blendName}>Danche</h3>
-                    <p className={styles.description}>Ethiopian hand-harvested blend densely packed with vibrant fruit notes</p>
+                    <div>
+                      <Image  src="/../public/assets/home/desktop/image-danche.png" alt="coffee grounds" width="255" height="193"  className={styles.img} />  
+                    </div>
+                    <div className={styles.textContainer}>
+                        <h3 className={styles.blendName}>Danche</h3>
+                        <p className={styles.description}>Ethiopian hand-harvested blend densely packed with vibrant fruit notes</p>  
+                    </div>
+                    
                 </div>
             </div>
             
