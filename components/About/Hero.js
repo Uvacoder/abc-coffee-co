6142438e6336f75f5a9e2b8933d949b54dfc36e4
SJellen <sjellen@me.com>
2021-07-07 12:13:49 -0400

    About/Hero mq's set

--- components/About/Hero.js
+++ components/About/Hero.js
@@ -5,9 +5,16 @@ export default function Hero() {
 
     return (
         <div className={styles.container}>
-            <div className={styles.imgContainer}>
-                <Image  src="/../public/assets/about/desktop/image-hero-whitecup.jpg" alt="coffee grinder" width="1280" height="450"  className={styles.img} />
+            <div className={styles.img}>
+                <Image  src="/../public/assets/about/desktop/image-hero-whitecup.jpg" alt="coffee grinder" width="1280" height="450" />
             </div>
+            <div className={styles.imgTablet}>
+                <Image  src="/../public/assets/about/tablet/image-hero-whitecup.jpg" alt="coffee grinder" width="690" height="400" />
+            </div>
+            <div className={styles.imgMobile}>
+                <Image  src="/../public/assets/about/mobile/image-hero-whitecup.jpg" alt="coffee grinder" width="500" height="600" />
+            </div>
+
             <div className={styles.textContainer}>
                 <h2>About Us</h2>
                 <p>Coffeeroasters began its journey of exotic discovery in 1999, highlighting stories of coffee from around the world. We have since been dedicated to bring the perfect cup - from bean to brew - in every shipment.</p>
