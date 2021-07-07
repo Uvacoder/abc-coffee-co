a9f5511dc58d1d545038848421c3788c558cb083
SJellen <sjellen@me.com>
2021-07-07 15:01:58 -0400

    About/Uncomprimising mq's set

--- components/About/Uncompromising.js
+++ components/About/Uncompromising.js
@@ -9,6 +9,12 @@ export default function Hero() {
               <div className={styles.imgContainer}>
                 <Image  src="/../public/assets/about/desktop/image-quality.jpg" alt="coffee grinder" width="445" height="474"  className={styles.img} />
             </div>
+            <div className={styles.imgContainerTablet}>
+                <Image  src="/../public/assets/about/tablet/image-quality.jpg" alt="coffee grinder" width="573" height="320"  className={styles.img} />
+            </div>
+            <div className={styles.imgContainerMobile}>
+                <Image  src="/../public/assets/about/mobile/image-quality.jpg" alt="coffee grinder" width="279" height="153"  className={styles.img} />
+            </div>
             <div className={styles.textContainer}>
                 <h2>Uncompromising quality</h2>
                 <p>Although we work with growers who pay close attention to all stages of harvest and processing, we employ, on our end, a rigorous quality control program to avoid over-roasting or baking the coffee dry. Every bag of coffee is tagged with a roast date and batch number. Our goal is to roast consistent, user-friendly coffee, so that brewing is easy and enjoyable.</p>
